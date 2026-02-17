4a16b13e856be629b8c6c30aba4efe8b8e009dfb
alonsods <alonsods@ciencias.unam.mx>
2026-02-17 12:59:14 -0600

    Primer commit

---
+++ src/main/kotlin/com/equipo7/proyectoAdopcion/ProyectoAdopcionApplication.kt
@@ -0,0 +1,11 @@
+package com.equipo7.proyectoAdopcion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProyectoAdopcionApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProyectoAdopcionApplication>(*args)
+}
---
+++ src/test/kotlin/com/equipo7/proyectoAdopcion/ProyectoAdopcionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.equipo7.proyectoAdopcion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProyectoAdopcionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

d091b4d50299ae9f168572c679cbe699242dabdf
edgarsago1 <edgarsago@ciencias.unam.mx>
2026-03-01 13:18:59 -0600

    Agregado el file usuario.sql , además de actualizar detalles del dominio para hacerlos coincidir

--- src/main/kotlin/com/equipo7/proyectoAdopcion/domain/Usuario.kt
+++ src/main/kotlin/com/equipo7/proyectoAdopcion/domain/Usuario.kt
@@ -1,12 +1,7 @@
-package com.equipo7.proyectoAdopcion.domain
-
-/**
- * Representa el modelo de dominio "Usuario" para el proyecto de adopción. 
- */
 data class Usuario(
     val id: String,
     var nombre: String,
     var email: String,
     var codigoPostal: String,
     var password: String? = null
-)
+    var token: String? = null
